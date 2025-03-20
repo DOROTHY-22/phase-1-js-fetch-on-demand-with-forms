795f3935f30cc2c18fde321cec8cca72f0b81a1a
DOROTHY CHEPKOECH <chepkoechdorothy209@gmail.com>
2025-03-20 12:30:58 +0300

    Fetch

--- src/index.js
+++ src/index.js
@@ -1,5 +1,48 @@
-const init = () => {
-  
-}
+/*const init = () => {
+   const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit",(event)=> {
+        event.preventDefault();
+       console.log(event.target.children[1].value);
+       const input = document.querySelector("input#searchByID");
+       
 
-document.addEventListener('DOMContentLoaded', init);
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response)=> response.json())
+       .then((data)=> {
+            console.log(data);
+        });
+    });
+  
+};
+
+// document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded',function(){
+    const form = document.querySelector('form').addEventListener('submit',(event)=>{
+        console.log(event.target.children[1].value);
+        event.preventDefault();
+    });
+});
+
+//input.addEventListener("submit",(event)=>{
+  //  console.log(event);
+//})*/
+
+const init = ()=>{
+    const inputForm = document.querySelector("form")
+    inputForm.addEventListener("submit",(event)=>{
+       console.log(event.target.children[1].value);
+        event.preventDefault();
+        const input = document.querySelector('input#searchByID');
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) =>response.json())
+        .then ((data)=>{
+            const title =document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        })
+    })
+}
+document.addEventListener('DOMContentLoaded',init);
+
+  
